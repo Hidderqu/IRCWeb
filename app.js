9199c70f64756b669ee4c37f7552e782a468af86
Q <qh5@hw.ac.uk>
2019-03-08 11:55:31 +0000

    Login + SignUp Pages

--- app.js
+++ app.js
@@ -39,4 +39,19 @@ app.get('/create', function (req, res) {
     })
 });
 
+app.get('/signup', function (req, res) {
+    fs.readFile('./pages/Create_account.html', function (err, data) {
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    })
+});
+
+app.get('/login', function (req, res) {
+    fs.readFile('./pages/Login.html', function (err, data) {
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    })
+});
 app.listen(4242, () => console.log('Started server on 4242'));
