32f464f7181d932499d695734c3d72431c490cf3
Q <qh5@hw.ac.uk>
2019-03-08 09:36:11 +0000

    Create Channel XHTML + Functionning Navbar

--- app.js
+++ app.js
@@ -6,9 +6,33 @@ const fs = require('fs');
 //Custoom modules
 var dateMod = require('./customModules/dateModule')
 
-//App code
+//App Routing
 app.get('/', function (req, res) {
     fs.readFile('./pages/index.xhtml', function (err, data) {
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    })
+});
+
+app.get('/channels', function (req, res) {
+    fs.readFile('./pages/channel_list.xhtml', function (err, data) {
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    })
+});
+
+app.get('/help', function (req, res) {
+    fs.readFile('./pages/help.xhtml', function (err, data) {
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    })
+});
+
+app.get('/create', function (req, res) {
+    fs.readFile('./pages/create.xhtml', function (err, data) {
         res.writeHead(200, {'Content-Type':'text/html'});
         res.write(data);
         res.end();
