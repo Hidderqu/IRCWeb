078e82516aa353fa0babab3a007e3434048ba961
aat4 <44607697+aat4@users.noreply.github.com>
2019-03-26 15:49:56 +0000

    created my own dir

--- pages/chatDir/chat.js
+++ pages/chatDir/chat.js
@@ -3,23 +3,26 @@
 
 //ircC.connectIRC("chatDir.freenode.net","6667");
 console.log("js called");
-
-
 var socket = null;
+var connected = false;
 
+function startChatting() {
+    
+}
 
 
 
 function connect()
 {   console.log("connected");
   //  socket = io('http://localhost:4343/node_modules/socket.io/lib/socket.js');
-    socket = io('http://localhost:4343');
    // var connection = socket.connect("http://localhost:4343/chatDir/chat",{reconnect: true});
+    socket = io('http://localhost:4343');
+
     socket = socket.connect("http://localhost:4343/chatDir/chat",{reconnect: true});
     console.log(socket);
-
+    connected = true;
     var chatlog = document.getElementById('chatbox');
-    socket.on('message', function(msg) {
+        socket.on('message', function(msg) {
         console.log("INSIDE ON");
 
         chatlog.innerHTML += "\n" + msg;
